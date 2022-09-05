97c45ae96b6109de44bf0ccc192270acec6d4883
Mercurial5 <dias.nespayev@gmail.com>
2022-09-05 10:59:05 +0600

    feat: add login

--- HeroCode/blueprints/auth/routes.py
+++ HeroCode/blueprints/auth/routes.py
@@ -1,4 +1,5 @@
 from sqlalchemy.exc import IntegrityError
+from flask.json import jsonify
 from flask import request
 import re
 
@@ -6,15 +7,17 @@ from HeroCode.blueprints.auth import auth
 from HeroCode.models import Users
 from HeroCode import db
 
+from utils import strings as s
+
 
 @auth.route('/register', methods=['POST'])
-def main():
+def register():
     username = request.form.get('username', None)
     email = request.form.get('email', None)
     password = request.form.get('password', None)
 
     if None in [username, email, password]:
-        return dict(status=False, reason='Not all data was given')
+        return dict(status=False, reason=s.missed_data)
 
     user = Users(username=username, email=email, password=password)
 
@@ -27,8 +30,23 @@ def main():
             # https://stackoverflow.com/questions/4666973/how-to-extract-the-substring-between-two-markers
             duplicate_key = re.search('\'users.(.+?)\'"', e.args[0]).group(1)
         except AttributeError:
-            return dict(status=False, reason='Unexpected error with duplicate key')
+            return dict(status=False, reason=s.unexpected_duplicate_error)
 
         return dict(status=False, reason=f'Duplicate key - {duplicate_key}')
 
     return dict(status=True)
+
+
+@auth.route('/login', methods=['POST'])
+def login():
+    username = request.form.get('username', None)
+    password = request.form.get('password', None)
+
+    if None in [username, password]:
+        return dict(status=False, reason=s.missed_data)
+
+    user = Users.get(username=username, password=password)
+    if user is None:
+        return dict(status=False)
+
+    return dict(status=True, user=user.serialize())
