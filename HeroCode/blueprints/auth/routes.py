92090ea3d9c820689157ae12ed4db3f410982359
Mercurial5 <dias.nespayev@gmail.com>
2022-09-06 09:09:06 +0600

    feat: confirm email

--- HeroCode/blueprints/auth/routes.py
+++ HeroCode/blueprints/auth/routes.py
@@ -1,3 +1,4 @@
+from itsdangerous import SignatureExpired, BadSignature
 from sqlalchemy.exc import IntegrityError
 from flask import request
 import re
@@ -6,8 +7,9 @@ from HeroCode.blueprints.auth import auth
 from HeroCode.models import Users
 from HeroCode import db
 
-from utils import strings as s
+from utils import token_serializer
 from utils import mailing
+from utils import strings
 
 
 @auth.route('/register', methods=['POST'])
@@ -17,7 +19,7 @@ def register():
     password = request.form.get('password', None)
 
     if None in [username, email, password]:
-        return dict(status=False, reason=s.missed_data)
+        return dict(status=False, reason=strings.missed_data)
 
     user = Users(username=username, email=email, password=password)
 
@@ -30,7 +32,7 @@ def register():
             # https://stackoverflow.com/questions/4666973/how-to-extract-the-substring-between-two-markers
             duplicate_key = re.search('\'users.(.+?)\'"', e.args[0]).group(1)
         except AttributeError:
-            return dict(status=False, reason=s.unexpected_duplicate_error)
+            return dict(status=False, reason=strings.unexpected_duplicate_error)
 
         return dict(status=False, reason=f'Duplicate key - {duplicate_key}')
 
@@ -45,10 +47,26 @@ def login():
     password = request.form.get('password', None)
 
     if None in [username, password]:
-        return dict(status=False, reason=s.missed_data)
+        return dict(status=False, reason=strings.missed_data)
 
     user = Users.get(username=username, password=password)
     if user is None:
-        return dict(status=False, reason=s.wrong_credentials)
+        return dict(status=False, reason=strings.wrong_credentials)
 
     return dict(status=True, user=user.serialize())
+
+
+@auth.route('/confirm-email/<token>')
+def confirm_email(token: str):
+    try:
+        email = token_serializer.decode_token(token, max_age=3600)
+    except SignatureExpired:
+        return dict(status=False, reason=strings.expired_token)
+    except BadSignature:
+        return dict(status=False, reason=strings.wrong_token)
+
+    user = Users.get(email=email)
+    user.is_active = 1
+    db.session.commit()
+
+    return dict(status=True)
--- utils/strings.py
+++ utils/strings.py
@@ -1,3 +1,5 @@
 missed_data = 'Not all data was given'
 unexpected_duplicate_error = 'Unexpected error with duplicate key'
 wrong_credentials = 'Wrong username or password'
+expired_token = 'Token has expired'
+wrong_token = 'Wrong token'
