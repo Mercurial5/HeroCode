f55c2f876f7623e8a489f3da419658765992ae30
Mercurial5 <dias.nespayev@gmail.com>
2022-09-06 09:09:37 +0600

    fix: circular import

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
-from utils.Mailing import Mailing
 from utils.TokenSerializer import TokenSerializer
-
-
-mailing = Mailing()
 token_serializer = TokenSerializer()
+
+
+from utils.Mailing import Mailing
+mailing = Mailing()
