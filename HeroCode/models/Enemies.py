cad06eaa31ea702c8c617850ac433f2dce26726f
Mercurial5 <dias.nespayev@gmail.com>
2022-09-11 14:11:40 +0600

    feat: add model `Enemies`

---
+++ HeroCode/models/Enemies.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from HeroCode.models import db
+
+
+class Enemies(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    hp = db.Column(db.Integer, nullable=False)
+
+    @staticmethod
+    def get(**kwargs) -> Enemies:
+        return Enemies.query.filter_by(**kwargs).first()
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'hp': self.hp
+        }
