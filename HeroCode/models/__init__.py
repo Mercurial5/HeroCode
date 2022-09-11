055a7b861eea393391596da70347f3904525fcc2
Mercurial5 <dias.nespayev@gmail.com>
2022-09-11 14:17:35 +0600

    chore: add migrations

--- HeroCode/models/__init__.py
+++ HeroCode/models/__init__.py
@@ -1,2 +1,5 @@
 from HeroCode import db
 from HeroCode.models.Users import Users
+from HeroCode.models.Enemies import Enemies
+from HeroCode.models.Problems import Problems
+from HeroCode.models.Tests import Tests
