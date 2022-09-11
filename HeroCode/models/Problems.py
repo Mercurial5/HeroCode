2dbc5d00f7e4c9425338ab7b1ae3309d2c53e414
Mercurial5 <dias.nespayev@gmail.com>
2022-09-11 14:11:49 +0600

    feat: add model `Problems`

---
+++ HeroCode/models/Problems.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from HeroCode.models import db
+
+
+class Problems(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    enemy_id = db.Column(db.Integer, db.ForeignKey('enemies.id'))
+    name = db.Column(db.String(255))
+    description = db.Column(db.Text)
+
+    @staticmethod
+    def get(**kwargs) -> list[Problems]:
+        return Problems.query.filter_by(**kwargs)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'enemy_id': self.enemy_id,
+            'name': self.name,
+            'description': self.description
+        }
